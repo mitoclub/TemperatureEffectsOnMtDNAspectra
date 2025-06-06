3359d519bdd35636ba19e81d7f4b626cbf814c0f
Alya Mikhailova <agmkhlva@gmail.com>
2025-06-06 11:39:15 +0200

    Analyses of neutral nuc content only on CYT B

--- code/Obtaining_codon_usage_from_MIDORI.R
+++ code/Obtaining_codon_usage_from_MIDORI.R
@@ -1,6 +1,3 @@
-library(here)
-library(tidyverse)
-
 if (!requireNamespace("seqinr", quietly = TRUE)) {
   install.packages("seqinr")
 }
@@ -13,11 +10,17 @@ if (!requireNamespace("systemPipeR", quietly = TRUE)) {
 if (!requireNamespace("Biostrings", quietly = TRUE)) {
   BiocManager::install("Biostrings")
 }
+if (!requireNamespace("coRdon", quietly = TRUE)) {
+  BiocManager::install("coRdon")
+}
 
 library(seqinr)
 library(ORFik)
 library(Biostrings) 
-library(systemPipeR)
+library(ShortRead)
+library(coRdon)
+library(here)
+library(tidyverse)
 
 # Set paths
 src_dir <- here("code")
@@ -27,38 +30,38 @@ plots_dir <- here(output_dir, "figures")
 tables_dir <- here(output_dir, "tables")
 
 
+CytBsequenses <- readDNAStringSet(here(data_dir, "MIDORI2_LONGEST_NUC_GB265_Cytb_BLAST.fasta"))
+CytBsequenses <- CytBsequenses[grepl("Chordata_7711", names(CytBsequenses))]
 
+start_codons <- subseq(CytBsequenses, 1, 3)
+valid_starts <- start_codons == "ATG"
+CytBsequensesonlyATG <- CytBsequenses[valid_starts]
+CytBsequensesonlyATG <- CytBsequensesonlyATG[width(CytBsequensesonlyATG) %% 3 == 0]
 
-  
-seqs <- readDNAStringSet(here(data_dir, "MIDORI2_LONGEST_NUC_GB265_Cytb_BLAST.fasta"))
-#problem_seqs <- vapply(seqs, function(x) any(uniqueLetters(x) %in% c("-", "N", "Y", "R")), logical(1))
-#names(seqs)[problem_seqs]
-seqs <- replaceAmbiguities(seqs)
-# Predict ORFs with ATG start codons
-orfs <- predORF(seqs,
-                n = 1,
-                mode = "orf",
-                type = "df",
-                startcodon = "ATG",
-                strand = "sense")
+codon_table <- codonTable(CytBsequensesonlyATG)
+CodonsCytB <- codonCounts(codon_table)
+dim(CodonsCytB)
+CodonsCytB <- data.frame(CodonsCytB)
+CodonsCytB$IDs <- getID(codon_table)
+CodonsCytB$Sequence <- as.character(CytBsequensesonlyATG)
+## count the number of A T G C in neutral positions of each gene (8 synon fourlfold codons
+CodonsCytB$NeutralA = as.numeric(CodonsCytB$CTA) + as.numeric(CodonsCytB$GTA) + as.numeric(CodonsCytB$TCA) + as.numeric(CodonsCytB$CCA)  + as.numeric(CodonsCytB$ACA)  + as.numeric(CodonsCytB$GCA)  + as.numeric(CodonsCytB$CGA)  + as.numeric(CodonsCytB$GGA)
+CodonsCytB$NeutralT = as.numeric(CodonsCytB$CTT) + as.numeric(CodonsCytB$GTT) + as.numeric(CodonsCytB$TCT) + as.numeric(CodonsCytB$CCT)  + as.numeric(CodonsCytB$ACT)  + as.numeric(CodonsCytB$GCT)  + as.numeric(CodonsCytB$CGT)  + as.numeric(CodonsCytB$GGT)
+CodonsCytB$NeutralG = as.numeric(CodonsCytB$CTG) + as.numeric(CodonsCytB$GTG) + as.numeric(CodonsCytB$TCG) + as.numeric(CodonsCytB$CCG)  + as.numeric(CodonsCytB$ACG)  + as.numeric(CodonsCytB$GCG)  + as.numeric(CodonsCytB$CGG)  + as.numeric(CodonsCytB$GGG)
+CodonsCytB$NeutralC = as.numeric(CodonsCytB$CTC) + as.numeric(CodonsCytB$GTC) + as.numeric(CodonsCytB$TCC) + as.numeric(CodonsCytB$CCC)  + as.numeric(CodonsCytB$ACC)  + as.numeric(CodonsCytB$GCC)  + as.numeric(CodonsCytB$CGC)  + as.numeric(CodonsCytB$GGC)
 
-
-extracted_seqs <- data.frame()
-
-for (i in seq_along(orfs$seqnames)) {
-  seqname <- as.character(orfs$seqnames[i])
-  subseq <- subseq(seqs[[seqname]], orfs$start[i], orfs$end[i])
-  df <- data.frame("Taxonomy" = seqname, "Sequence" = as.character(subseq), "Length" = orfs$width[i])
-  extracted_seqs <- rbind(extracted_seqs, df)
+CodonsCytB$Taxonomy <- strsplit(CodonsCytB$ID, ";")
+for (i in 1:nrow(CodonsCytB)) {
+  CodonsCytB$Type[i] <- CodonsCytB$Taxonomy[[i]][3]
+  CodonsCytB$Class[i] <- CodonsCytB$Taxonomy[[i]][4]
+  CodonsCytB$Species[i] <- CodonsCytB$Taxonomy[[i]][length(CodonsCytB$Taxonomy[[i]])]
 }
 
+CodonsCytB$Species <- gsub("_[0-9]+", "", CodonsCytB$Species)
+CodonsCytB$Class <- gsub("_[0-9]+", "", CodonsCytB$Class)
+CodonsCytB$Type <- gsub("_[0-9]+", "", CodonsCytB$Type)
+CodonsCytB$Taxonomy <- NULL
 
-Less <- extracted_seqs[extracted_seqs$Length > 99,]
 
+#write.table(CodonsCytB, file = here(data_dir, "Codons_of_CytB_gene_Chordata.txt"))
 
-OnlyChordata <- orfs %>% dplyr::filter(grepl("Chordata_7711", seqnames))
-OnlyFishes <- OnlyChordata %>% dplyr::filter(grepl("Actinopteri_186623", Name)) 
-OnlySharks <- OnlyChordata %>% dplyr::filter(grepl("Chondrichthyes_7777", Name))
-FishesCytB <- rbind(OnlySharks, OnlyFishes)
-
-write.csv(FishesCytB, here(data_dir, "MIDORI_WG_CYTB_FISHES.csv"))
